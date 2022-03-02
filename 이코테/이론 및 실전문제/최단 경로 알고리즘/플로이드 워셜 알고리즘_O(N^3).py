8c653288d9c7415337a3d6c1f9a4e8483c6ceb38
Hyunsoo Lee <wrkbrs@gmail.com>
2022-03-02 09:04:12 +0900

    플로이드 워셜 알고리즘

---
+++ 이코테/이론 및 실전문제/최단 경로 알고리즘/플로이드 워셜 알고리즘_O(N^3).py
@@ -0,0 +1,42 @@
+# 무한대 값
+INF = int(1e9)
+
+# 노드
+n = int(input())
+
+# 간선
+m = int(input())
+
+# 2차원 그래프
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선의 정보를 입력 받아서 초기화
+for i in range(m):
+    # a -> b 의 비용 c
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+# Dab = min(Dab, Dak + Dkb)
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print('INFINITY')
+        else:
+            print(graph[a][b], end=' ')
+    print()
+
+# 플로이드 워셜 알고리즘
+# 시간 복잡도: O(N^3)
