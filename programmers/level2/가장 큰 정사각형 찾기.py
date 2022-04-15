6837984cf869febc8e28404b1147b73a465198e9
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-15 15:40:33 +0900

    Update 파일명 변경 - 가장 큰 정사각형 찾기

--- programmers/level2/가장 큰 정사각형 찾기.py
+++ programmers/level2/가장 큰 정사각형 찾기.py
@@ -9,17 +9,28 @@ def solution(board):
 
     for i in range(1, len(board)):
         for j in range(1, len(board[0])):
-            smallest = sys.maxsize
-            for k in range(3):
-                y = i + dy[k]
-                x = j + dx[k]
-                print((i, j), (y, x))
-                smallest = min(smallest, board[y][x])
-            board[i][j] = smallest + 1
-            biggest = max(biggest, board[i][j])
+            if board[i][j] > 0:
+                smallest = sys.maxsize
+                for k in range(3):
+                    y = i + dy[k]
+                    x = j + dx[k]
+                    smallest = min(smallest, board[y][x])
+                board[i][j] = smallest + 1
+                biggest = max(biggest, board[i][j])
 
-    return biggest ** 2
+    if biggest == -sys.maxsize:
+        m = -1
+        for row in board:
+            m = max(row)
+            if m == 0:
+                return 0
+            else:
+                return 1
+
+    return biggest * biggest
 
 
-board = [[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]
+# board = [[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]
+# board = [[0, 0], [1, 0]]
+board = [[0, 0], [0, 1]]
 print(solution(board))
