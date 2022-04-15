35c41eb730c800892a9a7c46b1036b67b9411369
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-15 18:13:09 +0900

    Add H-Index - 프로그래머스

---
+++ programmers/level2/H-index.py
@@ -0,0 +1,23 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    citations = [-1] + citations
+    n = len(citations)
+
+    for i in range(1, n):
+        if i > citations[i]:
+            return i - 1
+
+    # 같은 수가 여러번 반복될 경우 None이 떠서 마지막 인덱스를 반환
+    return n - 1
+
+
+# print(solution([3, 0, 6, 1, 5]))
+# print(solution([10, 8, 5, 4, 3]))
+# print(solution([25, 8, 5, 3, 3]))
+# print(solution([0, 0, 0, 0]))
+# print(solution([1, 1, 1, 1]))
+print(solution([4, 4, 4]))
+
+
+# 참고
+# https://en.wikipedia.org/wiki/H-index
