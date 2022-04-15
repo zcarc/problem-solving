34457e9466fc535cde1ba2329cb86bd0ecb2cf7c
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-15 18:28:10 +0900

    Update 문제해설 H-Index

--- programmers/level2/H-index.py
+++ programmers/level2/H-index.py
@@ -7,7 +7,8 @@ def solution(citations):
         if i > citations[i]:
             return i - 1
 
-    # 같은 수가 여러번 반복될 경우 None이 떠서 마지막 인덱스를 반환
+    # 같은 수가 여러번 반복될 경우
+    # 반복되는 수와 인덱스가 일치한다면 None이 떠서 마지막 인덱스를 반환해야함
     return n - 1
 
 
@@ -16,7 +17,9 @@ def solution(citations):
 # print(solution([25, 8, 5, 3, 3]))
 # print(solution([0, 0, 0, 0]))
 # print(solution([1, 1, 1, 1]))
-print(solution([4, 4, 4]))
+print(solution([3, 3, 3, 3]))
+print(solution([3, 3, 3]))
+
 
 
 # 참고
