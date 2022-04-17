07e2fbcc8063b3d43eb3f17f64802b3a72138295
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-17 19:42:25 +0900

    Update 설명 - GCD,LCD 유클리드 호제법

--- ETC/GCD,LCD_유클리드 호제법.py
+++ ETC/GCD,LCD_유클리드 호제법.py
@@ -5,12 +5,36 @@ def gcd(a, b):
         return gcd(b, a % b)
 
 
-# a, b의 순서가 바뀌어도 답은 같다.
-print(gcd(192, 162))
+# 보통은 첫번째 인수에 더 큰 수가 오지만 두번째 인수에 더 큰 수가 와도 동일하게 동작한다.
+# 즉, a, b의 순서가 바뀌어도 답은 같다.
+print(gcd(162, 192))
 
 
 def lcd(a, b):
     return a * b // gcd(a, b)
 
 
-print(lcd(192, 162))
+print(lcd(162, 192))
+
+
+# gcd 호출 예시
+
+# A.
+# 1. call gcd(a = 10,  b = 20)
+# 2. return gcd(20, 10 % 20 (= 10))
+# 3. call gcd(20, 10)
+# 4. if a(20) % b(10) == 0:
+#        return b (= 10)
+
+# B.
+# 1. call gcd(162, 192)
+# 2. return gcd(192, 162 % 192 (= 162))
+# 3. call gcd(192, 162)
+# 4. return gcd(162, 192 % 162 (= 30))
+# 5. call gcd(162, 30)
+# 6. return gcd(30, 162 % 30 (= 12))
+# 5. call gcd(30, 12)
+# 6. return gcd(12, 30 % 12 (= 6))
+# 7. call gcd(12, 6)
+# 8. if a(12) % b(6) == 0:
+#        return b (= 6)
