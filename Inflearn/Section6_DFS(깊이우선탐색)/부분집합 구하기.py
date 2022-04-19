ff7def8093fd504648a87e0092256633b6e0e183
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-19 10:09:30 +0900

    Update 변수명 변경 - 부분집합 구하기 (Python3)

--- Inflearn/Section6_DFS(깊이우선탐색)/부분집합 구하기.py
+++ Inflearn/Section6_DFS(깊이우선탐색)/부분집합 구하기.py
@@ -4,19 +4,19 @@ def solution():
 
     ch = [0] * (n + 1)
 
-    def dfs(v):
-        if v == n + 1:
+    def dfs(level):
+        if level == n + 1:
             for i in range(1, n + 1):
                 if ch[i] == 1:
                     print(i, end=' ')
             print()
 
         else:
-            ch[v] = 1
-            dfs(v + 1)
+            ch[level] = 1
+            dfs(level + 1)
 
-            ch[v] = 0
-            dfs(v + 1)
+            ch[level] = 0
+            dfs(level + 1)
 
     dfs(1)
 
