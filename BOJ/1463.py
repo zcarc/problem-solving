b95c7ec00190ad64eb652ece65e85e5be561d927
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-19 19:14:43 +0900

    Add 1463 - 백준

---
+++ BOJ/1463.py
@@ -0,0 +1,18 @@
+x = int(input())
+
+
+def solution(x):
+    dp = [0] * (x + 1)
+
+    for i in range(2, x + 1):
+
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    return dp[x]
+
+
+print(solution(x))
