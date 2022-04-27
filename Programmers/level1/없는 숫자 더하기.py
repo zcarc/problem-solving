f809443c5a40701701e71a8d2834ab19a85501ba
Hyunsoo Lee <wrkbrs@gmail.com>
2022-04-27 19:31:38 +0900

    Add 없는 숫자 더하기 (Python3) - 프로그래머스

---
+++ Programmers/level1/없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+
+    total = 45
+    for e in numbers:
+        total -= e
+
+    return total
+
+
+print(solution([1, 2, 3, 4, 6, 7, 8, 0]))
+print(solution([5, 8, 4, 0, 6, 7, 9]))
