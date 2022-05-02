810efb78cab6a908e7fe1a6491b0883b8d8fb05a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-02 15:51:31 +0900

    Update 숫자의 표현 (Python3) - 프로그래머스

--- Programmers/level2/숫자의 표현.py
+++ Programmers/level2/숫자의 표현.py
@@ -1,20 +1,16 @@
 def solution(n):
-    sum_n = 0
+
     cnt = 0
-
     for i in range(1, n + 1):
-
-        sum_n = 0
-
+        total = 0
         for j in range(i, n + 1):
-
-            sum_n += j
-
-            if sum_n == n:
+            total += j
+            if total == n:
                 cnt += 1
                 break
-
-            elif sum_n > n:
+            # 이 부분을 추가하는 이유는 만약에 total의 크기가 n의 크기보다 더 크다면 답을 초과했으므로 더 이상 순회할 필요가 없고
+            # 이 부분을 고려하지 않으면 효율성 테스트에서 시간초과가 된다.
+            elif total > n:
                 break
 
     return cnt
