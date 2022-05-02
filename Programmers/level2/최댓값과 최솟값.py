471422e62be61c9be67c24725c69799a6ac4dd0a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-02 15:51:43 +0900

    Update 최댓값과 최솟값 (Python3) - 프로그래머스

--- Programmers/level2/최댓값과 최솟값.py
+++ Programmers/level2/최댓값과 최솟값.py
@@ -1,4 +1,10 @@
 def solution(s):
+    # split() 함수는 괄호 안에 값을 넣지 않으면 공백(스페이스, 탭, 엔터 등)을 기준으로 문자열을 나눈다.
+    list_s = list(map(int, s.split()))
+    return str(min(list_s)) + ' ' + str(max(list_s))
+
+
+def solution2(s):
     arr = list(map(int, s.split(' ')))
     answer = ''
     answer += str(min(arr))
@@ -8,6 +14,6 @@ def solution(s):
     return answer
 
 
-def solution2(s):
+def solution3(s):
     arr = list(map(int, s.split(' ')))
     return str(min(arr)) + ' ' + str(max(arr))
