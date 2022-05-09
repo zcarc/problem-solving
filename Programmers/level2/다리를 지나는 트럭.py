62fd9fc208bcad50645a65bd12e273548789f399
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-09 09:53:02 +0900

    Update 다리를 지나는 트럭 (Python3) - 프로그래머스

--- Programmers/level2/다리를 지나는 트럭.py
+++ Programmers/level2/다리를 지나는 트럭.py
@@ -21,17 +21,22 @@ def solution(bridge_length, weight, truck_weights):
             out_bridge = bridge_on.popleft()
             sum_bridge_on -= out_bridge
 
-    # 마지막에 brodge_on에 append된 트럭이 해당 다리의 길이만큼 지나야하므로
-    # 현재 경과시간 + 다리길이 만큼해줘서 답을 구할 수 있다.
     return time + bridge_length
 
 
 print(solution(2, 10, [7, 4, 5, 6]))
 print(solution(5, 5, [1, 1, 1, 1, 1, 1, 1, 1, 1]))
-print(solution(3, 1, [1,1,1]))
+print(solution(3, 1, [1, 1, 1]))
+
+
+# 설명
+# return time + bridge_length
+# 마지막에 bridge_on에 append된 트럭이 해당 다리의 길이만큼 지나야하므로
+# 현재 경과시간 + 다리길이 만큼해줘서 답을 구할 수 있다.
+
+# 이 문제는 원래 테스트케이스 5번에서 시간초과가 발생한 문제였었는데 ("다리를 지나는 트럭_시간초과.py")
+# sum(bridge_on) 를 사용하지 않고 다리에 올라간 합계를 구하는 변수 sum_bridge_on를 사용하니 시간초과가 되지 않고 풀 수 있었다.
 
 
 # 참고
 # https://mungto.tistory.com/4
-# 이 문제는 원래 테스트케이스 5번에서 시간초과가 발생한 문제였었는데 ("다리를 지나는 트럭_시간초과.py")
-# sum(bridge_on) 를 사용하지 않고 다리에 올라간 합계를 구하는 변수 sum_bridge_on를 사용하니 시간초과가 되지 않고 풀 수 있었다.
